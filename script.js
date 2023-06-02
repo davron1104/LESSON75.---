6c6b6deccb8e0ffd4b8b218bac9c61ac95fe93fb
Davron Gaziev <davron1104@yandex.com>
2023-06-02 23:43:21 +0500

    second

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// const num = new Function(3);
-// console.log(num);
+const num = new Function(3);
+console.log(num);
 
 
 
@@ -28,3 +28,30 @@ alex.hello();
 
 console.log(ivan);
 console.log(alex);
+
+
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+
+User.prototype.exit = function(name) {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const davron = new User('Davron', 36);
+const dilya = new User('Dilya', 32);
+
+davron.exit();
+
+davron.hello();
+dilya.hello();
+
+console.log(davron);
+console.log(dilya);
